73da254187e397c84dc9d757899cc676acd47818
VanGriffith <vc13grif@siena.edu>
2022-12-02 00:39:47 -0500

    First Attempt

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -1,17 +1,37 @@
-/*
-package DayX; --------------------------------------------------------------------------------------------------------------------------------
+package Day2; 
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class DayX {
-    public static void main(String[] args) { //---------------------------------------------------------------------------------------------------------------
+public class Day2 {
+
+    public static void main(String[] args) {
+        final int LOSS = 0;
+        final int DRAW = 3;
+        final int WIN = 6;
+
         try {
-            Scanner sc = new Scanner(new File("DayX/input.txt")); // -------------------------------------------------------------------------------------------
-            
+            Scanner sc = new Scanner(new File("Day2/input.txt")); 
+            int myPoints = 0;
             while (sc.hasNext()) {
-                String nextLine = sc.nextLine();
+                char oppShape = sc.next().charAt(0);
+                char myShape = sc.next().charAt(0);
+
+                myPoints += myShape - 'X' + 1;
+                int difference = myShape - oppShape;
+
+                if (difference % 3 == 0) {
+                    myPoints += WIN;
+                }
+                else if (difference % 3 == 1) {
+                    myPoints += LOSS;
+                }
+                else if (difference % 3 == 2) {
+                    myPoints += DRAW;
+                }
             }
+
+            System.out.println(myPoints);
             sc.close();
         }
         catch (FileNotFoundException e) {
@@ -19,5 +39,6 @@ public class DayX {
             e.printStackTrace();
         }
     }
+
 }
-*/
+
