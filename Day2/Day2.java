b3de5c1cce9e0ce9ad64701714c2823194281ce5
VanGriffith <vc13grif@siena.edu>
2022-12-02 00:03:40 -0500

    Made files

---
+++ Day2/Day2.java
@@ -0,0 +1,23 @@
+/*
+package DayX; --------------------------------------------------------------------------------------------------------------------------------
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class DayX {
+    public static void main(String[] args) { //---------------------------------------------------------------------------------------------------------------
+        try {
+            Scanner sc = new Scanner(new File("DayX/input.txt")); // -------------------------------------------------------------------------------------------
+            
+            while (sc.hasNext()) {
+                String nextLine = sc.nextLine();
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Input File Not Found");
+            e.printStackTrace();
+        }
+    }
+}
+*/
