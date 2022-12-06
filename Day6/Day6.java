7811a4eb66cec0cc1d4cc3cbf2431d0dcde67ab8
VanGriffith <vc13grif@siena.edu>
2022-12-05 23:51:05 -0500

    Create File

---
+++ Day6/Day6.java
@@ -0,0 +1,22 @@
+
+package Day6;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day6 {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("Day6/input.txt"));
+            
+            while (sc.hasNext()) {
+                String nextLine = sc.nextLine();
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Input File Not Found");
+            e.printStackTrace();
+        }
+    }
+}
