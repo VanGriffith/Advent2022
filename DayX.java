f8f21def31f6b670b064f5836eb3fd5fd369b6b5
VanGriffith <vc13grif@siena.edu>
2022-12-15 17:02:34 -0500

    Changed template file

--- DayX.java
+++ DayX.java
@@ -1,17 +1,14 @@
-/*
-package DayX; --------------------------------------------------------------------------------------------------------------------------------
+//package DayX;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class DayX {
-    public static void main(String[] args) { //---------------------------------------------------------------------------------------------------------------
+    public static void main(String[] args) {
         try {
-            Scanner sc = new Scanner(new File("DayX/input.txt")); // -------------------------------------------------------------------------------------------
+            Scanner sc = new Scanner(new File("DayX/input.txt"));
             
-            while (sc.hasNext()) {
-                String nextLine = sc.nextLine();
-            }
+            
             sc.close();
         }
         catch (FileNotFoundException e) {
@@ -20,4 +17,3 @@ public class DayX {
         }
     }
 }
-*/
