fcb7a1b520581c9c67410c478a76df6be2dabbb1
VanGriffith <vc13grif@siena.edu>
2022-12-20 09:54:43 -0500

    Create Cavern

--- Day17/Day17.java
+++ Day17/Day17.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Day17 {
+
+    public int[] cavern;
+
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(new File("Day17/input.txt"));
--- Day17/Tetranimo.java
+++ Day17/Tetranimo.java
@@ -1,8 +1,23 @@
 package Day17;
 
-public class Tetranimo {
-    
-    public Tetranimo() {
-        
+public abstract class Tetranimo {
+    public int xPos;
+    public int yPos;
+
+    public Tetranimo(int x, int y) {
+        this.xPos = x;
+        this.yPos = y;
     }
+
+    public void pushLeft() {
+        if (this.xPos > 0) this.xPos--;
+    }
+    public void pushRight() {
+        if (this.xPos + 1 < Cavern.caveWidth) this.xPos++;
+    }
+    public void fallDown() {
+        this.yPos--;
+    }
+
+    public abstract boolean collision();
 }
