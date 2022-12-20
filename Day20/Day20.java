da7e7f6ec55f9a5bd9a4ddf052293265b137c5a1
VanGriffith <vc13grif@siena.edu>
2022-12-20 13:18:27 -0500

    Complete Day20 part 1

--- Day20/Day20.java
+++ Day20/Day20.java
@@ -1,13 +1,35 @@
 package Day20;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Day20 {
+
+    public static CircularList list;
+
     public static void main(String[] args) {
         try {
-            Scanner sc = new Scanner(new File("DayX/input.txt"));
+            Scanner sc = new Scanner(new File("Day20/input.txt"));
             
+            Queue<Node> queue = new LinkedList<Node>();
+            list = new CircularList();
+
+            while (sc.hasNextInt()) {
+                Node node = new Node(sc.nextInt());
+                queue.offer(node);
+                list.addToEnd(node);
+            }
+
+            while (!queue.isEmpty()) {
+                
+                queue.poll().shift();
+            }
+
+            list.print();
+
+            System.out.println("Coordinates Sum: " + list.coordinatesSum());
             
             sc.close();
         }
